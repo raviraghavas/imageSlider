683653d1c47d7c6cb5925f5b8e5b6f309cb84353
Ravi Raghav <36546089+raviraghavas@users.noreply.github.com>
2019-04-08 01:19:21 +0530

    Update content.js

--- src/reducer/content.js
+++ src/reducer/content.js
@@ -8,7 +8,7 @@ const imageData = () => {
 		description:
 			'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
 		button: 'Read More',
-		image: 'F:/photos/chosen/DSC_5067.JPG',
+		image: 'https://i.imgur.com/ZXBtVw7.jpg',
 		user: 'Daniel',
 		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
 	},
